94696ba52208f304d75a06b144f4ef031f93c9d8
Fahmi Akbar <fahmialiasakbar@gmail.com>
2022-03-24 23:15:04 +0700

    Result

--- app/src/main/java/com/dinata/myquizapp/Constants.kt
+++ app/src/main/java/com/dinata/myquizapp/Constants.kt
@@ -1,13 +1,18 @@
 package com.dinata.myquizapp
 
 object Constants {
+
+    const val USER_NAME : String = "user_name"
+    const val TOTAL_QUESTION : String = "total_questions"
+    const val CORRECT_ANSWER : String = "correct_answer"
+
     fun getQuestions(): ArrayList<Question> {
         val questionList = ArrayList<Question>()
         val questionText = "What country does this flag belong to?"
         val que1 = Question(1, questionText, R.drawable.ic_flag_of_argentina,"Argentina","Moldova","Uruguay","Romania", 1)
         val que2 = Question(2, questionText, R.drawable.ic_flag_of_australia,"Australia","Fiji","New Zealand","Vanuatu", 1)
         val que3 = Question(3, questionText, R.drawable.ic_flag_of_belgium,"Croatia","Moldova","Germany","Belgium", 1)
-        val que4 = Question(4, questionText, R.drawable.ic_flag_of_brazil,"Argentina","Moldova","Uruguay","Romania", 1)
+        val que4 = Question(4, questionText, R.drawable.ic_flag_of_brazil,"Bolivia","Panama","Brazil","Armenia", 3)
         val que5 = Question(5, questionText, R.drawable.ic_flag_of_fiji,"Australia","Fiji","New Zealand","Vanuatu", 2)
         val que6 = Question(6, questionText, R.drawable.ic_flag_of_denmark,"Denmark","Austria","Swedi","Norway", 1)
         val que7 = Question(7, questionText, R.drawable.ic_flag_of_new_zealand,"Australia","Fiji","New Zealand","Vanuatu", 3)
--- app/src/main/java/com/dinata/myquizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/dinata/myquizapp/QuizQuestionsActivity.kt
@@ -1,5 +1,6 @@
 package com.dinata.myquizapp
 
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
@@ -14,6 +15,8 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
     private var mCurrentPosition : Int = 1
     private var mQuestionList : ArrayList<Question>? = null
     private var mSelectedOptionPosition : Int = 0
+    private var mUserName : String? = null
+    private var mCorrectAnswer : Int = 0
 
     private var progressBar : ProgressBar? = null
     private var tvProgress : TextView? = null
@@ -30,6 +33,8 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
+
+        mUserName = intent.getStringExtra(Constants.USER_NAME)
 
         progressBar = findViewById(R.id.progressBar)
         tvProgress = findViewById(R.id.tv_progess)
@@ -149,13 +154,20 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
                             setQuestion()
                         }
                         else -> {
-                            Toast.makeText(this, "You made id to the end", Toast.LENGTH_SHORT)
+                            val intent = Intent(this, ResultActivity::class.java)
+                            intent.putExtra(Constants.USER_NAME, mUserName)
+                            intent.putExtra(Constants.CORRECT_ANSWER, mCorrectAnswer)
+                            intent.putExtra(Constants.TOTAL_QUESTION, mQuestionList?.size)
+                            startActivity(intent)
+                            finish()
                         }
                     }
                 } else {
                     val question = mQuestionList?.get(mCurrentPosition - 1)
                     if (question!!.correctAnswer != mSelectedOptionPosition) {
                         answerView(mSelectedOptionPosition, R.drawable.wrong_option_border_bg)
+                    } else {
+                        mCorrectAnswer++
                     }
                     answerView(question.correctAnswer, R.drawable.correct_option_border_bg)
                     if (mCurrentPosition == mQuestionList!!.size) {
