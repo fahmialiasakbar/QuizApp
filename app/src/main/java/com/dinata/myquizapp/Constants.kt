807a2bfeb9e8395d759e64aebedc6193ed32ba45
Fahmi Akbar <fahmialiasakbar@gmail.com>
2022-03-24 19:12:34 +0700

    Can Do The Quiz

--- app/src/main/java/com/dinata/myquizapp/Constants.kt
+++ app/src/main/java/com/dinata/myquizapp/Constants.kt
@@ -4,18 +4,28 @@ object Constants {
     fun getQuestions(): ArrayList<Question> {
         val questionList = ArrayList<Question>()
         val questionText = "What country does this flag belong to?"
-        val que1 = Question(
-            1,
-            questionText,
-            R.drawable.ic_flag_of_argentina,
-            "Argentina",
-            "Moldova",
-            "Uruguay",
-            "Romania",
-            1
-        )
+        val que1 = Question(1, questionText, R.drawable.ic_flag_of_argentina,"Argentina","Moldova","Uruguay","Romania", 1)
+        val que2 = Question(2, questionText, R.drawable.ic_flag_of_australia,"Australia","Fiji","New Zealand","Vanuatu", 1)
+        val que3 = Question(3, questionText, R.drawable.ic_flag_of_belgium,"Croatia","Moldova","Germany","Belgium", 1)
+        val que4 = Question(4, questionText, R.drawable.ic_flag_of_brazil,"Argentina","Moldova","Uruguay","Romania", 1)
+        val que5 = Question(5, questionText, R.drawable.ic_flag_of_fiji,"Australia","Fiji","New Zealand","Vanuatu", 2)
+        val que6 = Question(6, questionText, R.drawable.ic_flag_of_denmark,"Denmark","Austria","Swedi","Norway", 1)
+        val que7 = Question(7, questionText, R.drawable.ic_flag_of_new_zealand,"Australia","Fiji","New Zealand","Vanuatu", 3)
+        val que8 = Question(8, questionText, R.drawable.ic_flag_of_kuwait,"Jordan","Kuwait","Saudi","Palestine", 2)
+        val que9 = Question(9, questionText, R.drawable.ic_flag_of_germany,"Croatia","Moldova","Germany","Belgium", 3)
+        val que10 = Question(10, questionText, R.drawable.ic_flag_of_india,"India","Japan","Code d'Ivory","Italy", 1)
+
 
         questionList.add(que1)
+        questionList.add(que2)
+        questionList.add(que3)
+        questionList.add(que4)
+        questionList.add(que5)
+        questionList.add(que6)
+        questionList.add(que7)
+        questionList.add(que8)
+        questionList.add(que9)
+        questionList.add(que10)
         return questionList
     }
 }
